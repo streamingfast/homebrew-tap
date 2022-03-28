c429967b70e48bfc576c7e0c8fa33e4a622ce8a1
goreleaserbot <goreleaser@streamingfast.io>
2022-03-28 13:19:03 -0400

    Brew formula update for sf-ethereum version v0.10.0-rc.3

--- sf-ethereum.rb
+++ sf-ethereum.rb
@@ -5,21 +5,21 @@
 class SfEthereum < Formula
   desc "Firehose on Ethereum stack"
   homepage "https://github.com/streamingfast/sf-ethereum"
-  version "0.10.0-rc.2"
+  version "0.10.0-rc.3"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/streamingfast/sf-ethereum/releases/download/v0.10.0-rc.2/sfeth_0.10.0-rc.2_macOS_x86_64.tar.gz"
-      sha256 "e113e282d16827e0a2d8573336451008a3f31e7cad13603c79cbb34ddbaaa49b"
+      url "https://github.com/streamingfast/sf-ethereum/releases/download/v0.10.0-rc.3/sfeth_0.10.0-rc.3_macOS_x86_64.tar.gz"
+      sha256 "0cdf5c3e24bc8ba5763d5af64a758bcca39d8c5a7251b6337f18d01f38114d19"
 
       def install
         bin.install "sfeth"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/streamingfast/sf-ethereum/releases/download/v0.10.0-rc.2/sfeth_0.10.0-rc.2_macOS_arm64.tar.gz"
-      sha256 "7f46626ed422816e3f5fba15c0adc57953ce31b6b49b195ebd9b33f096ce6568"
+      url "https://github.com/streamingfast/sf-ethereum/releases/download/v0.10.0-rc.3/sfeth_0.10.0-rc.3_macOS_arm64.tar.gz"
+      sha256 "343eee86fd3838d207fe2bf31b549cc4e44027c7ad037f216f22c8fb5fe85d7b"
 
       def install
         bin.install "sfeth"
@@ -29,8 +29,8 @@ class SfEthereum < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/streamingfast/sf-ethereum/releases/download/v0.10.0-rc.2/sfeth_0.10.0-rc.2_linux_x86_64.tar.gz"
-      sha256 "8a8825f606e703ca65a6afa15f89317e242afedec9a02878949e21cd65e35792"
+      url "https://github.com/streamingfast/sf-ethereum/releases/download/v0.10.0-rc.3/sfeth_0.10.0-rc.3_linux_x86_64.tar.gz"
+      sha256 "b5b985cc4e9cf4f5aea38bcb5d46a0336ef968cd60a91ffad84fd6e4736f6260"
 
       def install
         bin.install "sfeth"
