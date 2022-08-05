90da467a0793723edef7d667b5460de5d7dabdf9
goreleaserbot <goreleaser@streamingfast.io>
2022-08-05 13:16:24 -0400

    Brew formula update for substreams version v0.0.17

--- substreams.rb
+++ substreams.rb
@@ -5,21 +5,21 @@
 class Substreams < Formula
   desc "StreamingFast Substreams CLI"
   homepage "https://github.com/streamingfast/substreams"
-  version "0.0.16"
+  version "0.0.17"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/streamingfast/substreams/releases/download/v0.0.16/substreams_0.0.16_macOS_arm64.tar.gz"
-      sha256 "0e96d484a9bd8934009ef8f200b088323f3e2a34f48dddc2ef546a7c2c6229ca"
+      url "https://github.com/streamingfast/substreams/releases/download/v0.0.17/substreams_0.0.17_macOS_arm64.tar.gz"
+      sha256 "7358c1442c5c8b1743a9fadd38e73586f22c2268db65f52b974b20c9048fcdc3"
 
       def install
         bin.install "substreams"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/streamingfast/substreams/releases/download/v0.0.16/substreams_0.0.16_macOS_x86_64.tar.gz"
-      sha256 "ab12eb138debad7733b707228126fac91410a3e707152f5954edf654fd8444e8"
+      url "https://github.com/streamingfast/substreams/releases/download/v0.0.17/substreams_0.0.17_macOS_x86_64.tar.gz"
+      sha256 "5ea63eb9772604196e36b8c8188c36a46aa38eb945659d3b3612f2549a788475"
 
       def install
         bin.install "substreams"
@@ -29,8 +29,8 @@ class Substreams < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/streamingfast/substreams/releases/download/v0.0.16/substreams_0.0.16_linux_x86_64.tar.gz"
-      sha256 "237a16e44cd25b50412b53e251815c5b50fb7ffb72c963661f18238025168221"
+      url "https://github.com/streamingfast/substreams/releases/download/v0.0.17/substreams_0.0.17_linux_x86_64.tar.gz"
+      sha256 "2d349a5588efe4502cec10ecfcfd8771fca8d0896412a05840a440d3be6d420f"
 
       def install
         bin.install "substreams"
