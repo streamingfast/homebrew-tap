d4bf6e9c7538e95443121aac28d0e9bc71c957b7
goreleaserbot <goreleaser@streamingfast.io>
2023-02-01 15:29:23 -0500

    Brew formula update for firehose-ethereum version v1.3.3

--- firehose-ethereum.rb
+++ firehose-ethereum.rb
@@ -5,21 +5,21 @@
 class FirehoseEthereum < Formula
   desc "Firehose on Ethereum stack"
   homepage "https://github.com/streamingfast/firehose-ethereum"
-  version "1.3.2"
+  version "1.3.3"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/streamingfast/firehose-ethereum/releases/download/v1.3.2/fireeth_darwin_arm64.tar.gz"
-      sha256 "8d1a0691c0774e3acbeebd10c2634222dfc37380ee79db26c8be6564cb02fce3"
+      url "https://github.com/streamingfast/firehose-ethereum/releases/download/v1.3.3/fireeth_darwin_arm64.tar.gz"
+      sha256 "cb6e01fa3c59357a9688a07f7a3232e024e2e231c913fde32d2d42261a958373"
 
       def install
         bin.install "fireeth"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/streamingfast/firehose-ethereum/releases/download/v1.3.2/fireeth_darwin_x86_64.tar.gz"
-      sha256 "b99c7ee3bf85aecd4fca2d5cc17f8563a418de63d100770be27348d82da4c59b"
+      url "https://github.com/streamingfast/firehose-ethereum/releases/download/v1.3.3/fireeth_darwin_x86_64.tar.gz"
+      sha256 "5f430aa5abd73bafee482d0facb1e5250a5a21d8d6855b00bfb465077dd06769"
 
       def install
         bin.install "fireeth"
@@ -29,8 +29,8 @@ class FirehoseEthereum < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/streamingfast/firehose-ethereum/releases/download/v1.3.2/fireeth_linux_x86_64.tar.gz"
-      sha256 "38f87bb7af6a8b44d47332a3e5eceda084a001c713d705d935ffd4a29668d3e9"
+      url "https://github.com/streamingfast/firehose-ethereum/releases/download/v1.3.3/fireeth_linux_x86_64.tar.gz"
+      sha256 "0c8d9129e300c435d5d30975a9048231f83c33c3c6cffdf75c19eb1af85973a0"
 
       def install
         bin.install "fireeth"
