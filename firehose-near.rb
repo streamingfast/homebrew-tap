96e9141bb343c7b54a0de12e7db24b41580402f8
goreleaserbot <goreleaser@streamingfast.io>
2023-02-07 14:06:53 -0500

    Brew formula update for firehose-near version v1.0.3

--- firehose-near.rb
+++ firehose-near.rb
@@ -5,21 +5,21 @@
 class FirehoseNear < Formula
   desc "Firehose on NEAR stack"
   homepage "https://github.com/streamingfast/firehose-near"
-  version "1.0.2"
+  version "1.0.3"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/streamingfast/firehose-near/releases/download/v1.0.2/firenear_darwin_arm64.tar.gz"
-      sha256 "7c2ec5f9c9b5a70ee5a34b2e79fb12a0afb50555ec902d4e8819fd5fb3776b4c"
+      url "https://github.com/streamingfast/firehose-near/releases/download/v1.0.3/firenear_darwin_arm64.tar.gz"
+      sha256 "c538514abfcf7b87282cc9844ab5be47b56807a6df4650ae03008d6900011c60"
 
       def install
         bin.install "firenear"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/streamingfast/firehose-near/releases/download/v1.0.2/firenear_darwin_x86_64.tar.gz"
-      sha256 "bd46209c3edff0bfdcd0e8ec2a63c67a54f7a845969a762b060a1d69ad89dea5"
+      url "https://github.com/streamingfast/firehose-near/releases/download/v1.0.3/firenear_darwin_x86_64.tar.gz"
+      sha256 "17f8d2cfb336cfa38f78616ca786afc2fde0ce578429e66a0aa3f057642f3151"
 
       def install
         bin.install "firenear"
@@ -29,8 +29,8 @@ class FirehoseNear < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/streamingfast/firehose-near/releases/download/v1.0.2/firenear_linux_x86_64.tar.gz"
-      sha256 "9f523d31d7a4e3961bbbb81a1f0a4586a5c170462f74afeb2dff7b76202fd60b"
+      url "https://github.com/streamingfast/firehose-near/releases/download/v1.0.3/firenear_linux_x86_64.tar.gz"
+      sha256 "29ef5f6b0b03a0fe781f654ae8de9609a8522b9705ea6de7affff544b4d9f9c1"
 
       def install
         bin.install "firenear"
